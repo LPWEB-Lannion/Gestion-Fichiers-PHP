fcb026c5fefe4b953f3a5d82176a88bf9586224b
Lise Rochat <liserochat@live.fr>
2022-09-26 22:58:37 +0200

    exo01 done

--- exercice01.php
+++ exercice01.php
@@ -1,62 +1,127 @@
 <?php 
-
-//**********  DEBUT Declaration fonction moulinette() **********/
-function moulinette($file) {
+/*
+ * Main function of exercice
+ * parameters : 
+ * $fd is the file descriptor
+ * $headerKeysArray is the array of keys from the file header
+ * 
+ * return :
+ * $articles : array of articles with id as key
+ *
+*/
+function moulinette($fd, $headerKeys) {
 
     $articles = array();
 
     //Tant que la fin du fichier n'est pas atteint
-    while(! feof($file)) {
+    while (!feof($fd)) {
         
-        //Lecture du fichier ligne par ligne
-        $ligne = trim(fgets($file));
+        //Lecture du fichier ligne par ligne sans les caractères spéciaux (fonction trim())
+        $ligne = trim(fgets($fd));
 
-        $data = explode("\t", $ligne);
-
-        if(($data[7] + $data[8] + $data[9]) < 0) {
-            $en_stock = "N";
-        } 
-        else {
-            $en_stock = "O";
+        //Ignore les lignes vides
+        if (empty($ligne)) {
+            continue;
         }
 
-        $articles[$data[0]] = [
-            "designation" => $data[1],
-            "categorie" => $data[2],
-            "prix_de_vente" => $data[5],
-            "taux_de_tva" => $data[6],
-            "fournisseur" => $data[10],
-            "en_stock" => $en_stock
-        ];
+        //Extrait une chaine de caractère
+        $data = explode("\t", $ligne);
 
+        //Sauveagrde le CODE de l'article 
+        $id = $data[0];
+        //Recupere le tableau data à partir de l'indice 1
+        $values = array_slice($data, 1);
+
+        //intval -> interger value
+        if ((intval($values[6]) + intval($values[7]) + intval($values[8])) > 0) {
+            $stock = "true";
+        } 
+        else {
+            $stock = "false";
+        }
+
+        $i = 0;
+        // Construit le tableau d'article avec la clef provenant de headerKeys
+        foreach($values as $value) {
+            // Condition d'arret 
+            if (!isset($headerKeys[$i])) {
+                break;
+            }
+            $article[$headerKeys[$i]]= $value;
+            $i++;
+        }
+
+        $article['STOCK'] = $stock;
+        $articles[$id] = $article;
     }
-
+    
     return $articles;
 }
-   
 
-
-//**********  FIN Declaration fonction moulinette() ***********/
-
-//Ouverture du fichier 
-if(file_exists('produits.dat')) {
-
-    $ressource = fopen('produits.dat', 'r');
-
-    //Apelle de la fonction moulinette
-    $tableau_articles = moulinette($ressource);
-
-    var_dump($tableau_articles);
+/*
+ * Main function of exercice
+ * parameters : 
+ * $fd is the file descriptor
+ * 
+ * return :
+ * $headerFile : array of keys from the header, default values from exercice
+ *
+*/
+function getFileHeader($fd) {
     
-    //Compte le nombre d'article
-    $nombre_article = count($tableau_articles) - 2;
+    $headerFile = array();
 
-    echo "Nombre d'articles : " . $nombre_article;
+    if (!feof($fd)) {
+        $ligne = trim(fgets($fd));
 
-    //Fermeture du fichier
-    fclose($ressource);
+        if (empty($ligne)) {
+            return $headerFile;
+        }
 
+        $keys = explode("\t", $ligne);
+
+        foreach ($keys as $key) {
+            $headerFile[] = $key;
+        }
+
+        if ($headerFile[0] == 'CODE') {
+            $headerFile = array_slice($headerFile, 1);
+        }
+
+    }
+    else {
+        $headerFile = array(
+            "DESIGNATION",
+            "categorie",
+            "prix_de_vente",
+            "taux_de_tva",
+            "fournisseur",
+            "en_stock"
+        );
+    }
+    return $headerFile;
 }
 
 
+//Ouverture du fichier 
+if (file_exists('produits.dat')) {
+
+    //Ouverture du fichier en mode lecture
+    $fd = fopen('produits.dat', 'r');
+
+    //Apelle de la fonction getFileHeader
+    $headerKeys = getFileHeader($fd);
+
+    //Apelle de la fonction moulinette
+    $tableau_articles = moulinette($fd, $headerKeys);
+
+    var_dump($tableau_articles);
+    // var_dump($headerKeys);
+
+    echo "Nombre d'articles : " . count($tableau_articles);
+
+    //Fermeture du fichier
+    fclose($fd);
+
+}
 ?>
